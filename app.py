3f564cc07a85484971f928e9a045e7f12bf8f863
MarkYonan <91198609+MarkYonan@users.noreply.github.com>
2021-09-30 19:29:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ if female == True and male == False:
     st.plotly_chart(fig_female)
 
 if male == True and female == True:
-    st.error('Selecteer één optie')
+    st.error('SELECTEER EEN OPTIE')
 
 
 # #### ---
